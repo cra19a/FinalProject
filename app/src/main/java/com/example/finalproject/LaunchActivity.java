0495c6259a48117bdfae6cac398493063e193ce3
Clayton Arnett <cra19a@acu.edu>
2022-11-07 23:10:18 -0600

    Initial commit

---
+++ app/src/main/java/com/example/finalproject/LaunchActivity.java
@@ -0,0 +1,31 @@
+package com.example.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LaunchActivity extends AppCompatActivity {
+    private Button launch_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+
+        launch_button = (Button) findViewById(R.id.button);
+        launch_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainActivity();
+            }
+        });
+    }
+
+    public void openMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
