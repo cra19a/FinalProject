74a51f62824e88ad31b2533612e9bded6be50e85
cra19a <cra19a@acu.edu>
2022-12-08 09:22:09 -0600

    added functionality to calculate button... but still need to make output only go to 2 decimal places

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // total amount: editTextNumberDecimal
+    // people: editTextNumber
+    // percent to tip: editTextNumberDecimal2
+    // amount to tip (result): editTextNumber2
+
+    public void Calculate(View v) {
+        EditText amount = (EditText)findViewById(R.id.editTextNumberDecimal);
+        EditText people = (EditText)findViewById(R.id.editTextNumber);
+        EditText percent = (EditText)findViewById(R.id.editTextNumberDecimal2);
+
+        EditText total = (EditText)findViewById(R.id.editTextNumber2);
+
+        float n1 = Float.parseFloat(amount.getText().toString());
+        float n2 = Float.parseFloat(people.getText().toString());
+        float n3 = Float.parseFloat(percent.getText().toString());
+
+        float percentDecimal = n3/100;
+        float temp = n1*percentDecimal;
+
+        float result = (n1 + temp) / n2;
+        float resultFr = (result * 100) / 100.0F;
+
+        total.setText("Try tipping $" + resultFr + "!");
+
+        // amount + (amount*percentToTip) / people
     }
 }
